6599c41dcd620f69045c1c5b20a674466e8723bd
mohazain <gw.zain@gmail.com>
2021-04-21 11:32:27 +0300

    project 5 WordScramble

---
+++ project5/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by mZain on 21/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
