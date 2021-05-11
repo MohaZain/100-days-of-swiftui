cf91a897504816d660245ed73e5372e78ae8342b
mohazain <gw.zain@gmail.com>
2021-05-11 11:14:18 +0300

    project 6

---
+++ project6/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by mZain on 11/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
