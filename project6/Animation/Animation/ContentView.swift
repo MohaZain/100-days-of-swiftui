8f59640d732cbee8dcb95e19e7f05203512f3262
mohazain <gw.zain@gmail.com>
2021-05-11 11:25:17 +0300

    p6

--- project6/Animation/Animation/ContentView.swift
+++ project6/Animation/Animation/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var anmationAmount : CGFloat = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        Button("Tap me"){
+            self.anmationAmount += 1
+        }
+        .padding()
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+        .scaleEffect(anmationAmount)
+        .animation(.default)
+        .blur(radius: (anmationAmount - 1) * 3)
+        
     }
 }
 
