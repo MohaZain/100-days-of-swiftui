c5f3d867aeec57101bc35a67c4faee03ad8d9c8e
mohazain <gw.zain@gmail.com>
2021-05-23 10:25:01 +0300

    Project 2 GuessTheFlag with Project 6 animation Challenge

--- project2/GuessTheFlag/ContentView.swift
+++ project2/GuessTheFlag/ContentView.swift
@@ -4,6 +4,12 @@
 //
 //  Created by mZain on 05/04/2021.
 //
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by mZain on 05/04/2021.
+//
 
 import SwiftUI
 
@@ -24,9 +30,26 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var message = ""
     @State private var scoreCount = 0
-
+//    challange for project 6 rotaion , fade and shake error
+    @State private var ifCorrect = false
+    @State private var animationAmount = [0.0, 0.0, 0.0]
+    
+    @State private var animationFade = [true, true, true]
 //    var x = countries.count
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var attempts: Int = 0
+    
+    struct Shake: GeometryEffect {
+        var amount: CGFloat = 10
+        var shakesPerUnit = 3
+        var animatableData: CGFloat
+
+        func effectValue(size: CGSize) -> ProjectionTransform {
+            ProjectionTransform(CGAffineTransform(translationX:
+                amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+                y: 0))
+        }
+    }
     
     var body: some View {
         
@@ -50,42 +73,82 @@ struct ContentView: View {
                 ForEach(0..<3){ i in
                     Button(action: {
                         self.flagTapped(i)
+                        
                     })
                         {
-                        CapsuleImage(image_name : self.countries[i])
+                            CapsuleImage(image_name : self.countries[i])
+                                .rotation3DEffect(.degrees(animationAmount[i]), axis: (x: 0, y: 1, z: 0))
+                                .animation(
+                                    Animation.easeInOut(duration: 1)
+                                        .repeatCount(1, autoreverses: true)
+                                ).opacity(animationFade[i] ? 1 : 0.25)
+                                .modifier(Shake(animatableData: CGFloat(attempts)))
+                        
+                                
+                           
 //                        Image(self.countries[i])
 //                            .renderingMode(.original)
 //                            .clipShape(Capsule())
 //                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
 //                            .shadow(color: .black, radius: 2)
-                        }
+                    }
+                    
+                    
+                        
+
                     }
                 Text("Your Current Score is : \(scoreCount)")
+                    .foregroundColor(.white)
                 Spacer()
                     
                 }
             }
-        .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text(message), dismissButton: .default(Text("Continue")) {
-                self.askQuestion()
-            })
-        }
+//        .alert(isPresented: $showingScore) {
+//            Alert(title: Text(scoreTitle), message: Text(message), dismissButton: .default(Text("Continue")) {
+//                self.askQuestion()
+//            })
+//        }
+        
         }
     func flagTapped(_ number: Int) {
+        
+//        withAnimation(Animation.easeInOut(duration: 0.5)) {
+//                  self.animateOpacity = true
+//        }
         if number == correctAnswer {
             scoreTitle = "Correct"
             scoreCount += 1
             message = "Your score is :\(scoreCount)"
+            ifCorrect = true
+            withAnimation {
+                self.animationAmount[number] += 360
+                animationFade = [false, false, false]
+                self.animationFade[number] = true
+                
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.askQuestion()
+            }
+            
         } else {
             scoreTitle = "Wrong"
             message = "this is \(countries[number]) && You reach to  :\(scoreCount)"
             scoreCount = 0
+            ifCorrect = false
+            
+            withAnimation(.default) {
+                                self.attempts += 1
+                            }
+            
         }
-
+        
         showingScore = true
         }
     
     func askQuestion() {
+//        animationAmount = 0
+        animationFade = [true, true, true]
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
@@ -100,6 +163,7 @@ struct CapsuleImage: View {
             .clipShape(Capsule())
             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
             .shadow(color: .black, radius: 2)
+        
     }
 }
 
