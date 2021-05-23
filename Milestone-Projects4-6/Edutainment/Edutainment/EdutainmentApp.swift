87d25f12e9e0ec1fd424dad66181e1fff62dea61
mohazain <gw.zain@gmail.com>
2021-05-23 11:45:08 +0300

    Milestone: Projects 4-6

---
+++ Milestone-Projects4-6/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by mZain on 23/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
