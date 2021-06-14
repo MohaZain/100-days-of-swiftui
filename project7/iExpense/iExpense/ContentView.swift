805a77fbd50214f08d4557a756d21c69fb43e18a
mohazain <gw.zain@gmail.com>
2021-06-14 16:57:26 +0300

    project7

--- project7/iExpense/iExpense/ContentView.swift
+++ project7/iExpense/iExpense/ContentView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
 struct ContentView: View {
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+
+//    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+//    @State private var numbers = [Int]()
+//    @State private var currentNumber = 1
+//
+//    func removeRows(at offsets: IndexSet) {
+//        numbers.remove(atOffsets: offsets)
+//    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Save User") {
+            let encoder = JSONEncoder()
+
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
+//        NavigationView{
+//            VStack {
+//                List {
+//                    ForEach(numbers, id: \.self) {
+//                        Text("\($0)")
+//                    }
+//                    .onDelete(perform: removeRows)
+//                }
+//
+//                Button("Add Number") {
+//                    self.numbers.append(self.currentNumber)
+//                    self.currentNumber += 1
+//                }
+//            }
+//            .navigationBarItems(leading: EditButton())
+//        }
     }
 }
 
