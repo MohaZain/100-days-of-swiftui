a43675ceb1cfbe94531a7d0cc1b1b45b42166e7c
mohazain <gw.zain@gmail.com>
2021-06-16 16:45:42 +0300

    prject8

--- project8/Moonshot/Moonshot/ContentView.swift
+++ project8/Moonshot/Moonshot/ContentView.swift
@@ -6,11 +6,33 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink(destination: Text("Detail \(row)")) {
+                    Text("Row \(row)")
+                }
+            }
+            .navigationBarTitle("SwiftUI")
+        }
+//        ScrollView(.vertical) {
+//            VStack(spacing: 10) {
+//                ForEach(0..<100) {
+//                    Text("Item \($0)")
+//                        .font(.title)
+//                }
+//            }
+//            .frame(maxWidth: .infinity)
+//        }
+//        GeometryReader { geo in
+//            Image(systemName:"error")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: geo.size.width)
+//        }
     }
 }
 
